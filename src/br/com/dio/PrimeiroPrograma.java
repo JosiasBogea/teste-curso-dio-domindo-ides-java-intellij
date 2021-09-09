d2b62240756a025206e4d7cd4619f7ed66eec511
JosiasBogea <josiasbn@gmail.com>
2021-09-08 22:20:16 -0300

    firt commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,15 +2,62 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-/*
-        int a = 5;
-        int b = 3;
-*/
+        String nome;
+        Livro livro = new Livro("O problema dos 3 corpos ", 300);
+        System.out.println(livro);
 
-//        System.out.println("Hello, World! " + (a + b));
+    }
+
+}
+class Livro{
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return nome.equals(livro.nome) && numPag.equals(livro.numPag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPag);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
