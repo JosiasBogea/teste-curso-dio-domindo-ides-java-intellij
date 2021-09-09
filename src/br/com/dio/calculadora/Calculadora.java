2f5d84c72960dfdf0280b67c61ed648ffb330832
JosiasBogea <josiasbn@gmail.com>
2021-09-08 22:52:12 -0300

    criando calculadora

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -5,29 +5,34 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a, b;
+        double a, b;
 
         System.out.println("Digite o primeiro número: ");
         a = scan.nextInt();
         System.out.println("Digite o segundo número: ");
         b = scan.nextInt();
 
-        int soma = soma(a, b);
-        int subtracao = subtracao(a, b);
-        int multiplicacao = multiplicacao(a, b);
-        int divide = divisao(a, b);
+        double soma = soma(a, b);
+        double subtracao = subtracao(a, b);
+        double multiplicacao = multiplicacao(a, b);
+        double divide = divisao(a, b);
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divide);
 
     }
-    public static int soma(int a, int b){
+    public static double soma(double a, double b){
         return a + b;
     }
-    public static int subtracao(int a, int b){
+    public static double subtracao(double a, double b){
         return a - b;
     }
-    public static int multiplicacao(int a, int b){
+    public static double multiplicacao(double a, double b){
         return a * b;
     }
-    public static int divisao(int a, int b){
+    public static double divisao(double a, double b){
         return a / b;
     }
 
